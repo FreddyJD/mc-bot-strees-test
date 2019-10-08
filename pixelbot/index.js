7d6dfd0d791a447004b73d82e3d6c859aa29ab5d
Freddy <freddy.jd2@gmail.com>
2019-10-08 02:33:56 -0400

    asd

--- pixelbot/index.js
+++ pixelbot/index.js
@@ -1,11 +1,29 @@
+const cluster = require('cluster');
+const http = require('http');
 var fs = require('fs');
-
 const { exec } = require('child_process');
+
 
 var data = fs.readFileSync('./Nicks.txt', 'utf8');
 dataData = data.split('\n');
+const numCPUs = dataData.length;
+num = 0
 
-dataData.map((i) => {
-  exec(`node bot.js ${i}`);
-})
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    num++
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  exec(`node bot.js ${dataData[num]}`);
+}
+
+
 
