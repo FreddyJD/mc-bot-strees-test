c3dbbf699d18d76661c4b8f5e55ea3a6aa8a33d2
Freddy <freddy.jd2@gmail.com>
2019-10-08 18:07:45 -0400

    ad

--- pixelbot/bot.js
+++ pixelbot/bot.js
@@ -1,12 +1,26 @@
-var mineflayer = require('mineflayer');
-var AutoAuth = require('mineflayer-auto-auth');
+const mineflayer = require('mineflayer')
 
+if (process.argv.length < 3 || process.argv.length > 5) {
+  console.log('Usage : node multiple.js <host> <port>')
+  process.exit(1)
+}
 
-mineflayer.createBot({
-    username: process.argv[2],
-    host: 'mc.pixelbox.us',
-    port: 25565,
-    plugins: [ AutoAuth ],
-    AutoAuth: 'password',
-    version: "1.8"
-})
+let i = 0
+function next () {
+  if (i < process.argv[4]) {
+    i++
+    setTimeout(() => {
+      createBot(`pixelbox-bot${i}`)
+      next()
+    }, 100)
+  }
+}
+next()
+
+function createBot (name) {
+  mineflayer.createBot({
+    host: process.argv[2],
+    port: parseInt(process.argv[3]),
+    username: name
+  })
+}
